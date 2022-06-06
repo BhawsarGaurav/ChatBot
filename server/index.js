5e57191f4ed75ec053ab6fe405014f102b5a7439
BhawsarGaurav <gaurav.bhawsar@neosoftmail.com>
2022-06-06 11:53:21 +0530

    addedd index.ejs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotnev = require("dotenv");
 const cors = require("cors");
+const fs = require("fs");
 const chatBotRoutes = require("./Routes/chatBotRoutes");
 const app = express();
 
@@ -9,6 +10,10 @@ console.log(process.env.PORT);
 app.use(express.json());
 app.use(cors());
 app.use("/api/chatBot", chatBotRoutes);
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
 // console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 const port = process.env.PORT || 5000;
 const server = app.listen(
